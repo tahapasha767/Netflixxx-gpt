3ed2e13b43664bc94d59dbec162ddafdbc8c1d30
Taha <tahapasha121@gmail.com>
2024-11-28 12:31:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='w-20 h-10 bg-black'></div>
+    </div>
+  );
+}
+
+export default App;
