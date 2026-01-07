256ace0766828856316568823260fff518e393eb
tahapasha767 <110926026+tahapasha767@users.noreply.github.com>
2026-01-07 16:40:03 +0530

    Removed Provider from App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Body from './componenets/Body';
-import {Provider} from "react-redux"
-import appStore from './utilies/appStore';
 function App() {
   return (
-    <Provider store={appStore}>   
        <Body/>
-    </Provider>
-
   );
 }
 
