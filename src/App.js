a180145f528f7557dc46d63dbc7c9262d1be86cb
tahapasha767 <110926026+tahapasha767@users.noreply.github.com>
2026-01-07 10:49:44 +0530

    Fixed TC-1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Body from './componenets/Body';
-import {Provider} from "react-redux"
+import Loader from './componenets/Loader'; // Assuming Loader component is created
+import {Provider, useSelector} from "react-redux" // Added useSelector for accessing state
 import appStore from './utilies/appStore';
+
 function App() {
+  // Select the isLoading state from the Redux store
+  // Assuming a 'loader' slice exists in appStore with an 'isLoading' property
+  const isLoading = useSelector((state) => state.loader.isLoading); 
+
   return (
     <Provider store={appStore}>   
+       {/* Conditionally render the Loader component based on isLoading state */}
+       {isLoading && <Loader />}
        <Body/>
     </Provider>
-
   );
 }
 
